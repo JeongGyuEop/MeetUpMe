671ac81d5c3cb511b8c1b966e45bd1e4c45a1a42
JeongGyuEop <gueeop0219@naver.com>
2025-05-07 01:46:07 +0900

    Initial commit

---
+++ src/main/java/com/meetupme/MeetUpMeApplication.java
@@ -0,0 +1,13 @@
+package com.meetupme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetUpMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetUpMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meetupme/MeetUpMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetupme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetUpMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
